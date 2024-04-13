248d1da91ea73daa89a2a3a21ff93f35efca6ebe
poglodjan <100129357+poglodjan@users.noreply.github.com>
2024-04-13 13:51:38 +0200

    Update z1.py

--- z1.py
+++ z1.py
@@ -57,73 +57,7 @@ def etap1i2(obrotKonstruktora,rodzajToru,rodzajCheckpoint):
     builder.fill(rodzajCheckpoint)
 
 
-def etap3():
-    #tworzymy kulę ze szlamu
-    shapes.sphere(SLIME_BLOCK, pos(4, -30, 0), 1, ShapeOperation.REPLACE)
-    builder.teleport_to(pos(6, -32, 0))
-    builder.mark()
-    builder.face(EAST)
-    #ustawiamy pozycję pierwotną póżniej wrócimy w to miejsce konstruktorem i bęziemy dodawać ogień
-    builder.set_origin()
-    builder.move(FORWARD, 65)
-    builder.line(NETHERRACK)
-    #budowa platformy
-    builder.move(FORWARD, 4)
-    builder.mark()
-    builder.shift(2, 0, -4)
-    builder.fill(MOSS_STONE)
-    #wracamy aby dodać ogień
-    builder.teleport_to_origin()
-    builder.move(UP, 1)
-    for i in range(11):
-        builder.move(FORWARD, randint(3, 8))
-        builder.place(FIRE)
 
-
-def etap4():
-    #projektujemy skok wiary
-    builder.move(DOWN, 30)
-    builder.face(SOUTH)
-    builder.move(FORWARD, 6)
-    #budujemy studnię z wodą
-    builder.mark()
-    builder.shift(2, 2, 2)
-    builder.fill(BRICKS)
-    builder.shift(-1, 0, -1)
-    builder.place(WATER)
-    builder.move(FORWARD, 4)
-    builder.set_origin()
-    #pętla tworząca schody spieralne kończy swoje działanie kiedy gracz stanie na ostatniej platformie
-    while not (blocks.test_for_block(SEA_LANTERN, pos(0, -1, 0))):
-        builder.teleport_to_origin()
-        builder.face(SOUTH)
-        #budowanie schodów w każdej chwili może być przerwane kiedy gracz stanie na ostatniej platformie
-        for i in range(30):
-            if not blocks.test_for_block(SEA_LANTERN, pos(0, -1, 0)):
-                builder.place(POLISHED_GRANITE)
-                builder.move(FORWARD, 1)
-                builder.move(UP, 1)
-                builder.turn(RIGHT_TURN)
-            else:
-                break
-        builder.mark()
-        builder.shift(-2, 0, 2)
-        builder.fill(SEA_LANTERN)
-        builder.teleport_to_origin()
-        builder.face(SOUTH)
-        loops.pause(8000)
-        #podmieniamy schody na blok magmy
-        for i in range(30):
-            if not blocks.test_for_block(SEA_LANTERN, pos(0, -1, 0)):
-                builder.place(MAGMA_BLOCK)
-                builder.move(FORWARD, 1)
-                builder.move(UP, 1)
-                builder.turn(RIGHT_TURN)
-                loops.pause(1000)
-            else:
-                break
-    #gracz otrzymuje perłe kresu, która posłuży do teleportacji
-    mobs.give(mobs.target(NEAREST_PLAYER), ENDER_PEARL, 1)
 #pętla sterująca ładowaniem etapów oraz system checkpoint
 while True:
     if blocks.test_for_block(DIAMOND_BLOCK, pos(0, -1, 0)) and not czyEtap1:
@@ -133,22 +67,6 @@ while True:
         etap1i2(RIGHT_TURN,SLIME_BLOCK,GOLD_BLOCK)
         
 
-    elif blocks.test_for_block(GOLD_BLOCK, pos(0, -1, 0)) and not czyEtap2:
-        aktualnaPozycjaOdrodzenia = player.position()
-        czyEtap2 = True
-        etap1i2(LEFT_TURN,MAGENTA_STAINED_GLASS_PANE,BEDROCK)
-        
-
-    elif blocks.test_for_block(BEDROCK, pos(0, -1, 0)) and not czyEtap3:
-        aktualnaPozycjaOdrodzenia = player.position()
-        czyEtap3 = True
-        etap3()
-        
-
-    elif blocks.test_for_block(MOSS_STONE, pos(0, -1, 0)) and not czyEtap4:
-        aktualnaPozycjaOdrodzenia = player.position()
-        czyEtap4 = True
-        etap4()
         
 
    
